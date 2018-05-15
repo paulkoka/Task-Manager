a38e34954ee897b91894a101dd9d2c7493abed30
alspiat <alspiat@icloud.com>
2018-05-16 00:55:47 +0300

    Test commit: add Task class

---
+++ Task-Manager/Task.h
@@ -0,0 +1,13 @@
+//
+//  Task.h
+//  Task-Manager
+//
+//  Created by Алексей on 16.05.2018.
+//  Copyright © 2018 Paul. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Task : NSObject
+
+@end
