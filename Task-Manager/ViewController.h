10b6b5bf2282ca0048caefd46615eab3ac4adc5b
paulkoka <pavelkoka@gmail.com>
2018-05-15 20:55:31 +0300

    Initial Commit

---
+++ Task-Manager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Task-Manager
+//
+//  Created by paul on 5/15/18.
+//  Copyright © 2018 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
